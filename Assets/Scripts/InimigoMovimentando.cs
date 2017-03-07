c1e347491d0057404b7497e8c36a23ff0e120c96
Igor Altieres <igoraltieres@gmail.com>
2017-03-07 12:32:03 -0300

    Tentando Introduzir o inimigo com movimento FIZ DEPOIS DO VÍDEO 4 NÉLIO SORRY

---
+++ Assets/Scripts/InimigoMovimentando.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class InimigoMovimentando : MonoBehaviour
+{
+    public float velocidadeHorizontal;
+    public float velocidadeVertical;
+    public float min;
+    public float max;
+    public float espera;
+
+
+    void Start()
+    {
+        StartCoroutine(Move(max));
+    }
+
+    IEnumerator Move(float destino)
+    {
+        while (Mathf.Abs(destino - transform.localPosition.y) > 0.2f)
+        {
+            Vector3 direcao = (destino == max) ? Vector3.up : Vector3.down;
+            Vector3 velocidadeVetorial = direcao * velocidadeHorizontal;
+            transform.localPosition = transform.localPosition + velocidadeVetorial * Time.deltaTime;
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(espera);
+
+        destino = (destino == max) ? min : max;
+        StartCoroutine(Move(destino));
+    }
+}
